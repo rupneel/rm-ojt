83c4656624cfbc7f681100b248277a129dd7850e
thakkerstuti <thakkerstuti947@gmail.com>
2025-11-21 11:35:30 +0530

    Enhance logger with console output capability
    Added console handler to logger for INFO level output.

--- personal_finance/logger.py
+++ personal_finance/logger.py
@@ -10,3 +10,14 @@ formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 fh = logging.FileHandler(LOG_FILE, encoding="utf-8")
 fh.setFormatter(formatter)
 fh.setLevel(logging.DEBUG)
+
+# console handler
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+ch.setLevel(logging.INFO)
+
+if not logger.handlers:
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+__all__ = ["logger"]
